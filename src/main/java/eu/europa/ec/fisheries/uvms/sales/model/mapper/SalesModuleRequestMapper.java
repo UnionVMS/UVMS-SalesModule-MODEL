6627f4d772075a3e260d58c0b6557067c4c756db
stihft <stijn.hooft@lv.vlaanderen.be>
2017-05-26 11:19:14 +0200

    send validation result to Sales, put it in the response Sprint Backlog Item #138579 - Opzet message flow

--- src/main/java/eu/europa/ec/fisheries/uvms/sales/model/mapper/SalesModuleRequestMapper.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/sales/model/mapper/SalesModuleRequestMapper.java
@@ -1,9 +1,6 @@
 package eu.europa.ec.fisheries.uvms.sales.model.mapper;
 
-import eu.europa.ec.fisheries.schema.sales.SalesModuleMethod;
-import eu.europa.ec.fisheries.schema.sales.SalesQueryRequest;
-import eu.europa.ec.fisheries.schema.sales.SalesReportRequest;
-import eu.europa.ec.fisheries.schema.sales.ValidationQualityAnalysisType;
+import eu.europa.ec.fisheries.schema.sales.*;
 import eu.europa.ec.fisheries.uvms.rules.model.dto.ValidationResultDto;
 import eu.europa.ec.fisheries.uvms.sales.model.exception.SalesMarshallException;
 
@@ -13,7 +10,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class SalesModuleRequestMapper {
 
-    public static String createSalesReportRequest(String request, ValidationResultDto validationResultDto) throws SalesMarshallException {
+    public static String createSalesReportRequest(String request, ValidationResultDto validationResultDto, String pluginToSendResponseThrough) throws SalesMarshallException {
         List<ValidationQualityAnalysisType> validationQualityAnalysis = ValidationQualityAnalysisMapper.map(validationResultDto);
 
         SalesReportRequest salesReportRequest = new SalesReportRequest();
@@ -21,11 +18,12 @@ public class SalesModuleRequestMapper {
         salesReportRequest.setMethod(SalesModuleMethod.REPORT);
         salesReportRequest.getValidationResults().addAll(validationQualityAnalysis);
         salesReportRequest.setMessageValidationStatus(FLUXGPResponseMapper.map(validationResultDto));
+        salesReportRequest.setPluginToSendResponseThrough(pluginToSendResponseThrough);
 
         return JAXBMarshaller.marshallJaxBObjectToString(salesReportRequest);
     }
 
-    public static String createSalesQueryRequest(String query, ValidationResultDto validationResultDto) throws SalesMarshallException {
+    public static String createSalesQueryRequest(String query, ValidationResultDto validationResultDto, String pluginToSendResponseThrough) throws SalesMarshallException {
         List<ValidationQualityAnalysisType> validationQualityAnalysis = ValidationQualityAnalysisMapper.map(validationResultDto);
 
         SalesQueryRequest salesQueryRequest = new SalesQueryRequest();
@@ -33,8 +31,24 @@ public class SalesModuleRequestMapper {
         salesQueryRequest.setMethod(SalesModuleMethod.QUERY);
         salesQueryRequest.getValidationResults().addAll(validationQualityAnalysis);
         salesQueryRequest.setMessageValidationStatus(FLUXGPResponseMapper.map(validationResultDto));
+        salesQueryRequest.setPluginToSendResponseThrough(pluginToSendResponseThrough);
 
         return JAXBMarshaller.marshallJaxBObjectToString(salesQueryRequest);
     }
+
+    public static String createRespondToInvalidMessageRequest(String messageGuid, ValidationResultDto validationResultDto, String pluginToSendResponseThrough, String sender) throws SalesMarshallException {
+        List<ValidationQualityAnalysisType> validationQualityAnalysis = ValidationQualityAnalysisMapper.map(validationResultDto);
+
+        RespondToInvalidMessageRequest respondToInvalidMessageRequest = new RespondToInvalidMessageRequest();
+        respondToInvalidMessageRequest.setMethod(SalesModuleMethod.INVALID_MESSAGE);
+        respondToInvalidMessageRequest.getValidationResults().addAll(validationQualityAnalysis);
+        respondToInvalidMessageRequest.setMessageValidationStatus(FLUXGPResponseMapper.map(validationResultDto));
+        respondToInvalidMessageRequest.setPluginToSendResponseThrough(pluginToSendResponseThrough);
+        respondToInvalidMessageRequest.setSender(sender);
+        respondToInvalidMessageRequest.setMessageGuid(messageGuid);
+
+        return JAXBMarshaller.marshallJaxBObjectToString(respondToInvalidMessageRequest);
+    }
+
 
 }
--- src/test/java/eu/europa/ec/fisheries/uvms/sales/model/mapper/SalesModuleRequestMapperTest.java
+++ src/test/java/eu/europa/ec/fisheries/uvms/sales/model/mapper/SalesModuleRequestMapperTest.java
@@ -13,6 +13,7 @@ public class SalesModuleRequestMapperTest {
     @Test
     public void createSalesReportRequest() throws Exception {
         String report = "<report />";
+        String plugin = "FLUX";
 
         ValidationMessageType validationMessage1 = new ValidationMessageType();
         validationMessage1.setBrId("brId1");
@@ -34,13 +35,15 @@ public class SalesModuleRequestMapperTest {
                 "        <ns2:ID>brId1</ns2:ID>\n" +
                 "    </validationResult>\n" +
                 "    <messageValidationStatus>NOK</messageValidationStatus>\n" +
+                "    <pluginToSendResponseThrough>FLUX</pluginToSendResponseThrough>\n" +
                 "    <report>&lt;report /&gt;</report>\n" +
-                "</SalesReportRequest>\n", SalesModuleRequestMapper.createSalesReportRequest(report, validationResultDto));
+                "</SalesReportRequest>\n", SalesModuleRequestMapper.createSalesReportRequest(report, validationResultDto, plugin));
     }
 
     @Test
     public void createSalesQueryRequest() throws Exception {
         String report = "<query />";
+        String plugin = "FLUX";
 
         ValidationMessageType validationMessage1 = new ValidationMessageType();
         validationMessage1.setBrId("brId1");
@@ -62,8 +65,41 @@ public class SalesModuleRequestMapperTest {
                 "        <ns2:ID>brId1</ns2:ID>\n" +
                 "    </validationResult>\n" +
                 "    <messageValidationStatus>WOK</messageValidationStatus>\n" +
+                "    <pluginToSendResponseThrough>FLUX</pluginToSendResponseThrough>\n" +
                 "    <query>&lt;query /&gt;</query>\n" +
-                "</SalesQueryRequest>\n", SalesModuleRequestMapper.createSalesQueryRequest(report, validationResultDto));
+                "</SalesQueryRequest>\n", SalesModuleRequestMapper.createSalesQueryRequest(report, validationResultDto, plugin));
+    }
+
+    @Test
+    public void createRespondToInvalidMessageRequest() throws Exception {
+        String plugin = "FLUX";
+        String sender = "STIJN";
+        String messageGuid = "abc";
+
+        ValidationMessageType validationMessage1 = new ValidationMessageType();
+        validationMessage1.setBrId("brId1");
+        validationMessage1.setErrorType(ErrorType.ERROR);
+        validationMessage1.setLevel("L01");
+        validationMessage1.setMessage("message 1");
+
+        ValidationResultDto validationResultDto = new ValidationResultDto();
+        validationResultDto.setIsError(true);
+        validationResultDto.setValidationMessages(Lists.newArrayList(validationMessage1));
+
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                "<RespondToInvalidMessageRequest xmlns=\"eu.europa.ec.fisheries.schema.sales\" xmlns:ns2=\"urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:20\">\n" +
+                "    <method>INVALID_MESSAGE</method>\n" +
+                "    <validationResult>\n" +
+                "        <ns2:LevelCode>L01</ns2:LevelCode>\n" +
+                "        <ns2:TypeCode>ERR</ns2:TypeCode>\n" +
+                "        <ns2:Result>message 1</ns2:Result>\n" +
+                "        <ns2:ID>brId1</ns2:ID>\n" +
+                "    </validationResult>\n" +
+                "    <messageValidationStatus>NOK</messageValidationStatus>\n" +
+                "    <pluginToSendResponseThrough>FLUX</pluginToSendResponseThrough>\n" +
+                "    <sender>STIJN</sender>\n" +
+                "    <messageGuid>abc</messageGuid>\n" +
+                "</RespondToInvalidMessageRequest>\n", SalesModuleRequestMapper.createRespondToInvalidMessageRequest(messageGuid, validationResultDto, plugin, sender));
     }
 
 }
