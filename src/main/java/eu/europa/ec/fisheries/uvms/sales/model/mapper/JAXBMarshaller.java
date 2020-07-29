9607bb7f01b92206d2f1543d91283693142eaace
lsotiriadis <leontion.sotiriadis@sword-group.com>
2020-07-29 18:01:46 +0300

    [UNIONVMS-4739] JAXBMarshaller refactoring
    Signed-off-by: lsotiriadis <leontion.sotiriadis@sword-group.com>

--- src/main/java/eu/europa/ec/fisheries/uvms/sales/model/mapper/JAXBMarshaller.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/sales/model/mapper/JAXBMarshaller.java
@@ -11,27 +11,18 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
  */
 package eu.europa.ec.fisheries.uvms.sales.model.mapper;
 
+import eu.europa.ec.fisheries.uvms.commons.xml.AbstractJAXBMarshaller;
+import eu.europa.ec.fisheries.uvms.commons.xml.JAXBRuntimeException;
 import eu.europa.ec.fisheries.uvms.sales.model.exception.SalesMarshallException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.stream.StreamSource;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
-public class JAXBMarshaller {
-
-    static final Logger LOG = LoggerFactory.getLogger(JAXBMarshaller.class);
-
-    private static Map<String, JAXBContext> contexts = new HashMap<>();
+public class JAXBMarshaller extends AbstractJAXBMarshaller {
 
     private JAXBMarshaller() {}
 
@@ -41,22 +32,14 @@ public class JAXBMarshaller {
      * @param <T>
      * @param data
      * @return
-     * @throws
-     * SalesMarshallException
+     * @throws SalesMarshallException
      */
     public static <T> String marshallJaxBObjectToString(T data) throws SalesMarshallException {
         try {
-            JAXBContext jaxbContext = contexts.get(data.getClass().getName());
-            if (jaxbContext == null) {
-                jaxbContext = JAXBContext.newInstance(data.getClass());
-                contexts.put(data.getClass().getName(), jaxbContext);
-            }
-            Marshaller marshaller = jaxbContext.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            StringWriter sw = new StringWriter();
-            marshaller.marshal(data, sw);
-            return sw.toString();
-        } catch (JAXBException ex) {
+            Map<String,Object> properties = new HashMap<>();
+            properties.put(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            return marshallToString(data, properties);
+        } catch (JAXBException | JAXBRuntimeException ex) {
             throw new SalesMarshallException("Error when marshalling Object to String", ex);
         }
     }
@@ -71,10 +54,10 @@ public class JAXBMarshaller {
      * @throws
      * SalesMarshallException
      */
-    public static <R> R unmarshallTextMessage(TextMessage textMessage, Class clazz) throws SalesMarshallException {
+    public static <R> R unmarshallTextMessage(TextMessage textMessage, Class<R> clazz) throws SalesMarshallException {
         try {
             return unmarshallString(textMessage.getText(), clazz);
-        } catch (JMSException | SalesMarshallException ex) {
+        } catch (JMSException ex) {
             throw new SalesMarshallException("Error when unmarshalling text message", ex);
         }
     }
@@ -89,23 +72,11 @@ public class JAXBMarshaller {
      * @throws
      * SalesMarshallException
      */
-    public static <R> R unmarshallString(String text, Class clazz) throws SalesMarshallException {
+    public static <R> R unmarshallString(String text, Class<R> clazz) throws SalesMarshallException {
         try {
-            JAXBContext jc = contexts.get(clazz.getName());
-            if (jc == null) {
-                jc = JAXBContext.newInstance(clazz);
-                contexts.put(clazz.getName(), jc);
-            }
-            Unmarshaller unmarshaller = jc.createUnmarshaller();
-            StringReader sr = new StringReader(text);
-            StreamSource source = new StreamSource(sr);
-            long before = System.currentTimeMillis();
-            R object = (R) unmarshaller.unmarshal(source);
-            LOG.debug("Unmarshalling time: {}", (System.currentTimeMillis() - before));
-            return object;
-        } catch (JAXBException ex) {
+            return unmarshallTo(text,clazz);
+        } catch (JAXBException | JAXBRuntimeException ex) {
             throw new SalesMarshallException("Error when unmarshalling text", ex);
         }
     }
-
 }
